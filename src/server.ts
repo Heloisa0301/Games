f493fb5976d56186ce5959548fc1663621d0b619
Heloísa Scarante <helotognolli0301@gmail.com>
2026-05-14 20:13:37 -0300

    Configuração de CORS

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import { routes } from './routes';
 import { initDb } from './database';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
